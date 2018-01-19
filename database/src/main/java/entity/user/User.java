dc6d5b580be64fb9730a2535ce1734e6f60db261
Aleksey <aleksey.v.sh@gmail.com>
2018-01-19 21:57:55 +0300

    Pom changes. Checkstyle included. Style warnings fixed

--- database/src/main/java/entity/user/User.java
+++ database/src/main/java/entity/user/User.java
@@ -1,42 +1,20 @@
 package entity.user;
 
-
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
 
 /**
  * @author a.shestovsky
  */
 @Getter
 @Setter
-//@AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 public class User {
     private Long id;
     private String firstName;
     private String lastName;
-//    private String email;
-//    private String password;
-//    private String phone;
-//    private String address;
-//    private LocalDate registrationDate;
-//    private Role role;
-
-//    public User(String firstName, String lastName, String email, String password,
-//                String phone, String address, LocalDate registrationDate, Role role) {
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.email = email;
-//        this.password = password;
-//        this.phone = phone;
-//        this.address = address;
-//        this.registrationDate = registrationDate;
-//        this.role = role;
-//    }
 
     public User(Long id, String firstName, String lastName) {
         this.id = id;
