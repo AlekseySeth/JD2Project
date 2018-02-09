4b22e29fed18a06e4c11ebd31295a4528121f9df
Aleksey <aleksey.v.sh@gmail.com>
2018-02-09 11:08:21 +0300

    Product dao

--- database/src/main/java/dao/ProductDao.java
+++ database/src/main/java/dao/ProductDao.java
@@ -29,11 +29,32 @@ public class ProductDao extends GenericDao<Product> {
     }
 
     public List<Product> findByCategory(Category category) {
-        return null;
+        Session session = SessionFactoryManager.getSessionFactory().openSession();
+        List<Product> productsByCategory = session
+                .createQuery("select p from Product where p.category = :productCategory", Product.class)
+                .setParameter("productCategory", category)
+                .getResultList();
+        session.close();
+        return productsByCategory;
     }
 
-    public List<Brand> findByBrand(Brand brand) {
-        return null;
+    public List<Product> findByBrand(Brand brand) {
+        Session session = SessionFactoryManager.getSessionFactory().openSession();
+        List<Product> productsByBrand = session
+                .createQuery("select p from Product where p.brand = :productBrand", Product.class)
+                .setParameter("productBrand", brand)
+                .getResultList();
+        session.close();
+        return productsByBrand;
     }
 
+    public List<Product> findByTitle(String title) {
+        Session session = SessionFactoryManager.getSessionFactory().openSession();
+        List<Product> products = session
+                .createQuery("select p from Product where p.title like %:productTitle%", Product.class)
+                .setParameter(":productTitle", title)
+                .getResultList();
+        session.close();
+        return products;
+    }
 }
