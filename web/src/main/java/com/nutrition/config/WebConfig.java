a41867a4f255b53221b6fa6715c5d2f1d5710961
Aleksey <aleksey.v.sh@gmail.com>
2018-02-27 17:11:45 +0300

    RequestMappingHandlerAdapter added. I18n works

--- web/src/main/java/com/nutrition/config/WebConfig.java
+++ web/src/main/java/com/nutrition/config/WebConfig.java
@@ -1,11 +1,15 @@
 package com.nutrition.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author a.shestovsky
@@ -16,6 +20,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Import(value = {ThymeleafConfig.class, InternationalizationConfig.class})
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
+
+    @PostConstruct
+    public void init() {
+        requestMappingHandlerAdapter.setIgnoreDefaultModelOnRedirect(true);
+    }
 
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
