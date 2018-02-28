6e2a72c8a74dd23253e114f7c0ad74d921673cc1
Aleksey <aleksey.v.sh@gmail.com>
2018-02-28 09:33:58 +0300

    Language controller

--- web/src/main/java/com/nutrition/controller/LoginController.java
+++ web/src/main/java/com/nutrition/controller/LoginController.java
@@ -1,25 +1,13 @@
 package com.nutrition.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@SessionAttributes("language")
 public class LoginController {
 
     @GetMapping("/login")
     public String showLoginPage() {
         return "login";
-    }
-
-    @PostMapping("/language")
-    public String selectLanguage(String language, Model model, @RequestHeader("Referer") String referer) {
-        model.addAttribute("language", language);
-        return "redirect:" + referer;
     }
 }
