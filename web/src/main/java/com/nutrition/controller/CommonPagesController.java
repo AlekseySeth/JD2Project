3a4afa934dd8a876bb37769e4624c3fc08dc59a5
Aleksey <aleksey.v.sh@gmail.com>
2018-02-28 11:11:54 +0300

    Common pages html + controller

---
+++ web/src/main/java/com/nutrition/controller/CommonPagesController.java
@@ -0,0 +1,23 @@
+package com.nutrition.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CommonPagesController {
+
+    @GetMapping("/articles")
+    public String showArticlesPage() {
+        return "articles";
+    }
+
+    @GetMapping("/delivery-info")
+    public String showDeliveryInfoPage() {
+        return "delivery-info";
+    }
+
+    @GetMapping("/contact-us")
+    public String showContactUsPage() {
+        return "contact-us";
+    }
+}
