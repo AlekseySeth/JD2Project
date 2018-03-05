eb7b0002b8642430e4eb333abb1f324c29b1785c
Aleksey <aleksey.v.sh@gmail.com>
2018-03-05 23:03:29 +0300

    My account + security (w/o password encoding)

--- web/src/main/java/com/nutrition/controller/AccountController.java
+++ web/src/main/java/com/nutrition/controller/AccountController.java
@@ -6,18 +6,15 @@ import com.nutrition.entity.user.User;
 import com.nutrition.order.OrderService;
 import com.nutrition.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -46,12 +43,6 @@ public class AccountController {
         return "login";
     }
 
-//    @PostMapping("/login")
-//    public String loginSystemUser(String email, String password, RedirectAttributes user) {
-//
-//        return "redirect:/my-account";
-//    }
-
     @GetMapping("/registration")
     public String showRegistrationPage() {
         return "registration";
@@ -67,7 +58,9 @@ public class AccountController {
     }
 
     @GetMapping("/my-account")
-    public String showMyAccountPage(User user, Model model) {
+    public String showMyAccountPage(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", user);
         List<Order> allOrdersByUser = orderService.findAllByUser(user);
         model.addAttribute("allOrdersByUser", allOrdersByUser);
         return "my-account";
